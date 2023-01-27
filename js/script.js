8f43b39af7c1fe457620d1f1847db897e1cc2041
Andrea Raguso <info@andrearaguso.com>
2023-01-27 06:30:20 +0100

    esercizio finito

--- js/script.js
+++ js/script.js
@@ -181,7 +181,50 @@ const { createApp } = Vue
                 status: 'received'
             }
             ],
-        }       
+        },  
+        {
+            name: 'Michele',
+            avatar: '_1',
+            visible: true,
+            messages: [
+                {
+                    date: '10/01/2020 15:30:55',
+                    message: 'Hai portato a spasso il cane?',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 15:50:00',
+                    message: 'Ricordati di dargli da mangiare',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 16:15:22',
+                    message: 'Tutto fatto!',
+                    status: 'received'
+                }
+            ],
+        },
+        {
+            name: 'Fabio',
+            avatar: '_2',
+            visible: true,
+            messages: [{
+                date: '20/03/2020 16:30:00',
+                message: 'Ciao come stai?',
+                status: 'sent'
+            },
+                {
+                    date: '20/03/2020 16:30:55',
+                    message: 'Bene grazie! Stasera ci vediamo?',
+                    status: 'received'
+                },
+                {
+                    date: '20/03/2020 16:35:00',
+                    message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                    status: 'received'
+                }
+            ],
+        },     
         ] 
 
         }
@@ -209,9 +252,11 @@ const { createApp } = Vue
 
         
         replyMessage(){
+            let hours = new Date().getHours();
+            let minutes = new Date().getMinutes();
             let contact = this.contacts[this.currentUser].messages
             contact.push({
-                date: 'bho',
+                date: hours + ':' + minutes,
                 message: 'Sono d\'accordo con te',
                 status: 'received'
             })
